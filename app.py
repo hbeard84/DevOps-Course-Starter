85a195f036c6bc87e96cca3396518ac9bc443fc9
Hayley <hbeard@live.co.uk>
2021-05-07 17:04:15 +0100

    Ability to read cards from my trello Board

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
+import requests
+from flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    items = session.get_items()
-    return render_template("index.html", todos = items)
+    response=requests.get(f"https://api.trello.com/1/boards/{Config.TRELLO_BOARD_ID}/cards?key={Config.TRELLO_API_KEY}&token={Config.TRELLO_API_SECRET}")
+    cards=response.json()
+    return render_template("index.html", todos = cards)
 
 
 @app.route('/add-todo', methods=["POST"])
